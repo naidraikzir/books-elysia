3f10ade6fdc305f9624245dee04dfa015ec66a1f
Rizki Ardian <radeite8@gmail.com>
2024-08-21 20:01:21 +0700

    fix schema imports and add get a collection with books

--- src/db.ts
+++ src/db.ts
@@ -1,16 +1,16 @@
 import { Database } from 'bun:sqlite'
 import { DB_NAME } from '@/constants'
 import { drizzle } from 'drizzle-orm/bun-sqlite'
-import { books } from './schemas/books.schema'
-import { collections } from './schemas/collections.schema'
-import { collectionsOfBooks } from './schemas/collectionsOfBooks.schema'
-import { users } from './schemas/users.schema'
+import * as books from './schemas/books.schema'
+import * as collections from './schemas/collections.schema'
+import * as collectionsOfBooks from './schemas/collectionsOfBooks.schema'
+import * as users from './schemas/users.schema'
 
 const schema = {
-  books,
-  collections,
-  collectionsOfBooks,
-  users,
+  ...books,
+  ...collections,
+  ...collectionsOfBooks,
+  ...users,
 }
 
 const sqlite = new Database(DB_NAME)
--- src/routes/collections/[id]/books/index.ts
+++ src/routes/collections/[id]/books/index.ts
@@ -1,13 +1,84 @@
 import type { ElysiaApp } from '@/.'
 import { db } from '@/db'
 import { jwtHandler } from '@/middlewares/jwt'
+import { selectBookSchema } from '@/schemas/books.schema'
+import { collections } from '@/schemas/collections.schema'
 import { collectionsOfBooks } from '@/schemas/collectionsOfBooks.schema'
+import { eq } from 'drizzle-orm'
 import { t } from 'elysia'
 
 export default (app: ElysiaApp) =>
   app
 
     .use(jwtHandler)
+
+    .get(
+      '',
+      async ({ set, params: { id } }) => {
+        const collection = await db.query.collections.findFirst({
+          where: eq(collections.id, id),
+          with: {
+            books: {
+              with: {
+                book: true,
+              },
+            },
+          },
+        })
+
+        if (!collection) {
+          set.status = 404
+          return 'Not Found'
+        }
+
+        return collection
+      },
+      {
+        params: t.Object({
+          id: t.String(),
+        }),
+        response: {
+          200: t.Object(
+            {
+              id: t.String({ default: '00000000-0000-0000-0000-000000000000' }),
+              name: t.Union([t.Null(), t.String()], { default: 'name' }),
+              userId: t.Union([t.Null(), t.String()], {
+                default: '00000000-0000-0000-0000-000000000000',
+              }),
+              timestamp: t.Union([t.Null(), t.String()], {
+                default: '2000-01-01 00:00:00',
+              }),
+              books: t.Array(
+                t.Object(
+                  {
+                    collectionId: t.String(),
+                    bookId: t.String(),
+                    book: selectBookSchema,
+                  },
+                  {
+                    default: {
+                      collectionId: '00000000-0000-0000-0000-000000000000',
+                      bookId: '00000000-0000-0000-0000-000000000000',
+                      book: {
+                        id: '00000000-0000-0000-0000-000000000000',
+                        name: 'name',
+                        author: 'author',
+                        cover: 'cover.webp',
+                        timestamp: '2000-01-01 00:00:00',
+                      },
+                    },
+                  },
+                ),
+              ),
+            },
+            {
+              description: 'OK',
+            },
+          ),
+          404: t.String({ default: 'Not Found', description: 'Not Found' }),
+        },
+      },
+    )
 
     .post(
       '',
@@ -22,6 +93,7 @@ export default (app: ElysiaApp) =>
             })),
           )
           .returning()
+
         return collection
       },
       {
