8e7b27220974e8f16f1b1f34fbb957c40003d99c
Rizki Ardian <radeite8@gmail.com>
2024-08-24 12:34:44 +0700

    handle not found error

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import { plugins } from '@/plugins'
 import { Elysia } from 'elysia'
 
-const app = new Elysia().use(plugins).listen(Bun.env.PORT || 3000)
+const app = new Elysia()
+  .use(plugins)
+  .onError(({ code }) => {
+    if (code === 'NOT_FOUND') {
+      return new Response(null, {
+        status: 404,
+      })
+    }
+  })
+  .listen(Bun.env.PORT || 3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
